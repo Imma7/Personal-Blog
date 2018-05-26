4b73f2ef95fb483ec18e6256faba2622e6ac2989
imma <immamugambi@gmail.com>
2018-05-26 17:29:15 +0300

    Added writer class

--- app/models.py
+++ app/models.py
@@ -7,3 +7,22 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Writer(db.Model):
+    __tablename__ = 'writers'
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    email = db.Column(db.String)
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
