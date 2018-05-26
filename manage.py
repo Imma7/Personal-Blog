e2f4b367e30cf1ad3d4d2a1d05caca867b833661
imma <immamugambi@gmail.com>
2018-05-26 17:17:37 +0300

    Import classes and create a shell context

--- manage.py
+++ manage.py
@@ -1,8 +1,15 @@
 from app import create_app
 from flask_script import Manager,Server
+from app import create_app,db
+from app.models import User, Writer, Post, Comment, Subscription
 
 #Creating app instance
 app = create_app('development')
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Writer = Writer, Post = Post, Comment = Comment, Subscription = Subscription )
+
 
 if __name__ == '__main__':
     manager.run()
