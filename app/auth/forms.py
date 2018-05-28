12a60d6aac69ba45b9fbcb0709b4c0bc25969e15
imma <immamugambi@gmail.com>
2018-05-28 06:59:01 +0300

    Added authentication registration form

---
+++ app/auth/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,SubmitField
+from wtforms.validators import Required,Email,EqualTo
+from wtforms import ValidationError
+from ..models import User
+
+class RegistrationForm(FlaskForm): #Creating Registration form class
+    email = StringField('Your Email Address',validators=[Required(),Email()]) #input field email passing in required and email validators 
+    username = StringField('Username',validators=[Required()]) #input username field
+    password = PasswordField('Password',validators=[Required(),EqualTo('password_confirm',message="Passwords must match")]) #input password field
+    password_confirm = PasswordField('Confirm Passwords',validators=[Required()]) #input password confirm field
+    submit = SubmitField('Sign Up')
