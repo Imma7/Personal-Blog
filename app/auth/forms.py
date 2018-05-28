74db8f2c6554eeacfbcc11e12b747d89607bd510
imma <immamugambi@gmail.com>
2018-05-28 07:00:10 +0300

    Added authentication login form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -10,3 +10,11 @@ class RegistrationForm(FlaskForm): #Creating Registration form class
     password = PasswordField('Password',validators=[Required(),EqualTo('password_confirm',message="Passwords must match")]) #input password field
     password_confirm = PasswordField('Confirm Passwords',validators=[Required()]) #input password confirm field
     submit = SubmitField('Sign Up')
+
+#Login Input Fields 
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators=[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+    #input fields for the users email,password and a boolean to confirm whether the user wants to be logged out after the session
